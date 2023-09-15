49c840d99db0071ee9013ee6f6f1147091d595df
Shivam Singh <100526291+shivamsan@users.noreply.github.com>
2023-09-15 20:54:44 +0530

    Update Task2_NumGuess.java

--- Task2_NumGuess.java
+++ Task2_NumGuess.java
@@ -26,44 +26,50 @@ public class Task2_NumGuess {
 		
 		System.out.println("\t\t\t\t\t\tRules\n\t\t\t\t\t\t-----");
 		
-		System.out.println("1. This game generates a random number between 1-100 and you will have to guess it right");
-		System.out.println("2. There will be in total 10 rounds");
-		System.out.println("3. For each correct guess, you will gain 10 points and will advance to next guess");
-		System.out.println("4. If you guess all consicutive 10 guesses correctly you will be declared the winner");
-		System.out.println("5. Any wrong guess would result into termination of game and you will have to start over");
-		System.out.println("6. To still continue playing the game press 1");
-		System.out.println("7. To exit out of the game press 0\n\n");
+		System.out.println("1. This game generates a random number between 1-100 and you will have to guess it right.");
+		System.out.println("2. There will be in total 10 rounds.");
+		System.out.println("3. For each correct guess, you will gain 10 points and will advance to next guess.");
+		System.out.println("4. If you guess all consicutive 10 guesses correctly you will be declared the winner.");
+		System.out.println("5. Any wrong guess would result into termination of game and you will have to start over.");
+		System.out.println("6. To still continue playing the game press 1.");
+		System.out.println("7. To exit out of the game press 0.\n\n");
 		
-		System.out.println("Let the game begin\n");
+		System.out.println("Let the game begin.\n");
 		int ch =1;
 		int points=0;
-		
 		int round=1;
 		do
 		{
+			int ran = r.nextInt(100);
+			//int ran =5;
+			System.out.println("----------------------------------------------------------------------------------------------------------\n");
+
 			System.out.println("Round "+round);
 			round=round+1;
 			System.out.println("Your current score = "+points);
-			System.out.println("Generating number...\n");
-			System.out.println("Guess the number ###");
+			System.out.println("\nGenerating number...");
+			System.out.println("Generated number is : ###\n");
+			System.out.print("Make a random guess :  ");
 			
 			int n=sc.nextInt();
-			int ran = r.nextInt(100);
-			//int ran =5;
 			
 			if (n==ran)
 			{
 				System.out.println("You Guessed it right.");
-				if(round==10)
+				
+				points= points+10;
+				
+				if(round==11)
 				{
 					System.out.println("You just scored a 100!! \nCongrats!! YOU WON THE GAME!!");
 					break;
 				}
 				System.out.println("You earned +10 points Advancing to next round...\n\n");
-				points= points+10;
-				
+				System.out.println("----------------------------------------------------------------------------------------------------------\n");
+
 				continue;
 			}
+			
 			else if(n<ran)
 			{
 				System.out.println("You guessed too low");
@@ -77,15 +83,14 @@ public class Task2_NumGuess {
 			{
 				System.out.println("Entered number is out limit. Enter number between 1 to 100");
 			}
-			System.out.println("The number was "+ran);
+			System.out.println("The actual number was "+ran);
 			System.out.println("Do you still wanna continue. \nTo still continue playing the game press 1. \nTo exit the game press 0.");
 			ch=sc.nextInt();
+			System.out.println("----------------------------------------------------------------------------------------------------------\n");
+
 			
-		}while(ch==1 && round <=10);
+		}while(ch==1 && round <11);
 		System.out.println("Thank you for playing. \nEnding Game...");
 		sc.close();
-		
-		
 	}
-
 }
